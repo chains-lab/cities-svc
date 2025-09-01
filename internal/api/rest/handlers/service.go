abaf29ccb8ebc1510e9e5d950922e1d9ff078bf2
trpdjke <yehorushakov@gmail.com>
2025-09-01 07:46:31 +0300

    add rest

---
+++ internal/api/rest/handlers/service.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/chains-lab/cities-svc/internal/app"
+	"github.com/chains-lab/cities-svc/internal/config"
+	"github.com/chains-lab/logium"
+)
+
+type Service struct {
+	app *app.App
+	log logium.Logger
+	cfg config.Config
+}
+
+func NewService(cfg config.Config, log logium.Logger, a *app.App) Service {
+	return Service{
+		app: a,
+		log: log,
+		cfg: cfg,
+	}
+}
+
+func (s Service) Log(r *http.Request) logium.Logger {
+	return s.log
+}
